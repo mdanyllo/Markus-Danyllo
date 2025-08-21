9b76a1adc3a5fb22cbfe6f5278f824a56704d4af
Seu Nome <markusdanyllo07@gmail.com>
2025-08-20 21:01:45 -0300

    responsividade do hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -24,16 +24,16 @@ export default function Hero() {
   }
 
   return (
-    <div className="flex flex-col items-center md:mt-40 mt-30 px-4 sm:px-8 md:px-16 lg:px-24">
-      <div className="flex flex-col md:flex-row items-center md:items-center w-full md:gap-30 gap-10">
-        <div className="flex flex-col items-center justify-center w-full md:w-1/2">
+    <div className="flex flex-col items-center md:mt-40 mt-30 px-4 sm:px-8 md:w-full md:px-16 lg:px-24">
+      <div className="flex flex-col items-center w-full gap-10 md:flex-row md:gap-30 md:justify-center">
+        <div className="flex flex-col items-center w-full md:items-start md:w-1/3">
           <p className="text-black text-lg sm:text-xl capitalize">Olá, {nome ? nome : "meu amigo(a)"}! Me chamo</p>
-          <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-2">Markus Danyllo</h1>
+          <h1 className="text-4xl md:text-6xl font-bold mb-2">Markus Danyllo</h1>
           <p className="text-black text-base sm:text-lg">
             Desenvolvedor de Software e Técnico de Robótica
           </p>
         </div>
-        <div className="relative w-48 sm:w-64 md:w-80 h-48 sm:h-64 md:h-80 rounded-3xl overflow-hidden hover:scale-105 transition-transform duration-500 animate-fadeIn">
+        <div className="flex flex-col items-center w-48 sm:w-64 md:w-80 h-48 sm:h-64 md:h-80 rounded-3xl">
         <Image
           src="/perfil.JPG"
           alt="Markus programando"
@@ -42,34 +42,34 @@ export default function Hero() {
           height={600}      
           className="w-48 sm:w-64 md:w-80 rounded-3xl hover:scale-105 transition-transform duration-500 animate-fadeIn object-cover"
         />
+          <section className="flex justify-center items-center gap-6  mt-4 sm:gap-8 animate-fadeIn">
+            <Link
+              className="flex items-center text-black text-sm sm:text-base font-medium gap-2 hover:scale-110 transition-transform duration-300"
+              href="https://github.com/mdanyllo"
+            >
+              <Image
+                src="/github.png"
+                alt="GitHub"
+                width={20}
+                height={24}
+              />
+              GitHub
+            </Link>
+            <Link
+              className="flex items-center text-black text-sm sm:text-base font-medium gap-2 hover:scale-110 transition-transform duration-300"
+              href="https://www.linkedin.com/in/markusdanyllo"
+            >
+              <Image
+                src="/linkedin.png"
+                alt="LinkedIn"
+                width={20}
+                height={24}
+              />
+              LinkedIn
+            </Link>
+          </section>
+        </div>
       </div>
-      </div>
-      <section className="w-1/2 flex justify-center items-center gap-6 sm:justify-end md:ml-52 mt-4 sm:gap-8 animate-fadeIn">
-        <Link
-          className="flex items-center text-black text-sm sm:text-base font-medium gap-2 hover:scale-110 transition-transform duration-300"
-          href="https://github.com/mdanyllo"
-        >
-          <Image
-            src="/github.png"
-            alt="GitHub"
-            width={24}  // w-6 equivale a 24px
-            height={24}
-          />
-          GitHub
-        </Link>
-        <Link
-          className="flex items-center text-black text-sm sm:text-base font-medium gap-2 hover:scale-110 transition-transform duration-300"
-          href="https://www.linkedin.com/in/markusdanyllo"
-        >
-          <Image
-            src="/linkedin.png"
-            alt="LinkedIn"
-            width={24}
-            height={24}
-          />
-          LinkedIn
-        </Link>
-      </section>
       <button className="flex items-center justify-center text-black font-medium mt-20 px-6 py-3 border-2 border-[#ff9500] rounded-full cursor-pointer transition-colors duration-300 hover:bg-[#f58f00] hover:text-white">
         Conheça o meu trabalho
       </button>
