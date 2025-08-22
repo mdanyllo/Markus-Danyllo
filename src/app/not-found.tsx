e1e804053bf53e7504edc178963f2e3ee120de96
mdanyllo <markusdanyllo07@gmail.com>
2025-08-22 14:59:14 -0300

    alterações

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ import { div } from "framer-motion/client";
 
 export default function NotFound() {
     return (
-        <div className="flex flex-col items-center justify-center w-full mt-20">
+        <div className="flex flex-col items-center justify-center w-full mt-50">
             <h1 className="md:text-4xl text-3xl text-center font-medium">404</h1>
             <h2 className="md:text-2xl font-medium">Ops! Acho que você errou. Essa página não existe</h2>
         </div>
